dbdb49c5dfa5b73dc7feab92b91d3c0e700cd263
Mariel Báez Sánchez <marielbaezsanchez991@@email.com>
2024-02-16 15:47:06 -0600

    02 App Career - Career Model

--- seani/career/apps.py
+++ seani/career/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class LibraryConfig(AppConfig):
+class CareerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'library'
-    verbose_name = 'biblioteca'
+    name = 'career'
+    verbose_name = 'carrera'
