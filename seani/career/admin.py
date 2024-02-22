e162845a2300c43f99c40ebb684a623f7d8aae4f
Mariel Báez Sánchez <marielbaezsanchez991@@email.com>
2024-02-22 10:21:06 -0600

    end 02 App Career - Career Model

--- seani/career/admin.py
+++ seani/career/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Career
 
-admin.site.register(Career)
+class CareerAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'level']
+    list_filter = ['level']
+
+admin.site.register(Career, CareerAdmin)
